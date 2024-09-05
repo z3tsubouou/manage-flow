c4f8369525507760d6ea8129f29d1517e7eedd6f
Davgatseren <25279427+z3tsubouou@users.noreply.github.com>
2024-09-05 13:32:00 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./styles.css";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+import { NextUIProvider } from "@nextui-org/system";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <NextUIProvider>
+        <RouterProvider router={router} />
+      </NextUIProvider>
+    </StrictMode>,
+  );
+}
---
+++ src/routes/__root.tsx
@@ -0,0 +1,31 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import React from "react";
+
+const TanStackRouterDevtools = import.meta.env.PROD
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+      // Lazy load in development
+      import("@tanstack/router-devtools").then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    );
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
